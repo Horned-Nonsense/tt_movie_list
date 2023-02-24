9291c92952e6585ec15dabf57f92d9491d8605c8
Artem Shylo <artem.shylo@proarea.com>
2023-02-24 19:36:58 +0200

    Init

---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+import 'muvie_list_app.dart';
+
+void main() {
+  runApp(const MuvieListApp());
+}
+
---
+++ lib/muvie_list_app.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class MuvieListApp extends StatelessWidget {
+  const MuvieListApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Muvie List',
+      home: Container(),
+    );
+  }
+}
